1fa87579c260e040d3c73fe57caea9b7fbd1581d
diploper76 <cbcdaijee145@gmail.com>
2025-02-15 20:15:49 +0545

    print python program and working conditon

---
+++ app.py
@@ -0,0 +1,20 @@
+# print("Hello welcome to python")
+# name = input("Enter your name: ")
+# # print(name)
+# a = 10
+# b = 33
+# if b > a:
+#   print("b is greater than a")
+# elif a == b:
+#   print("a and b are equal")
+
+number =input ("Enter a number")
+print(number)
+nextNumber = input("Enter a another number")
+print(nextNumber)
+if(number > nextNumber):
+    print(number)
+elif nextNumber > number:  # Complete the elif condition
+    print(nextNumber)
+else:
+     print("Both numbers are equal") 
